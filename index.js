e039f18426c6cca6d93176ad940bfaf05ae539c4
ssudo <ramasedang@gmail.com>
2023-03-30 14:20:25 +0700

    fix

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import Joi from 'joi';
-import * as rsa from './rsa.js';
+import rsa from './rsa.js';
 import fs from 'fs';
+import { PrismaClient } from '@prisma/client';
 
-let pubkey = JSON.parse(fs.readFileSync("publicKey.json"));
-let privkey = JSON.parse(fs.readFileSync("privateKey.json"));
-pubkey = JSON.parse(rsa.fromBase64(pubkey));
-privkey = JSON.parse(rsa.fromBase64(privkey));
+let pubkey = JSON.parse(fs.readFileSync('publicKey.json'));
+let privkey = JSON.parse(fs.readFileSync('privateKey.json'));
+pubkey = JSON.parse(await rsa.fromBase64(pubkey));
+privkey = JSON.parse(await rsa.fromBase64(privkey));
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const schema = Joi.object().keys({
-  nama: Joi.string().required(),
-  email: Joi.string().email().required(),
-  asal_institut: Joi.string().required(),
-  no_hp: Joi.string().required(),
-  alamat: Joi.string().required(),
-  jenis_lomba: Joi.string().required(),
-  link_pembayaran: Joi.string().uri()
-});
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/daftar', (req, res) => {
+app.post('/daftar', async (req, res) => {
   try {
-    schema.validate(req.body);
-    let plaintext = JSON.stringify(req.body);
-    let ciphertext = rsa.encrypt(plaintext, pubkey);
-    ciphertext = rsa.toBase64(ciphertext);
-    let decrypted = rsa.decrypt(rsa.fromBase64(ciphertext), privkey);
-    console.log(decrypted);
+    const x = req.body;
+    const { data } = x;
+    let chipertext = await rsa.fromBase64(data);
+    const decryptedData = await rsa.decrypt(chipertext, privkey);
+    const parsedData = JSON.parse(decryptedData);
+
+    //save data to database
+    await prisma.peserta.create({
+      data: parsedData,
+    });
+
     res.status(200).json({
-        status: 'success',
-        message: 'Data berhasil disimpan',
-        data: ciphertext,
+      status: 'success',
+      message: 'Data berhasil didaftarkan',
     });
   } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
+    console.log(error);
+  }
+});
+
+app.get('/peserta', async (req, res) => {
+  try {
+    const data = await prisma.peserta.findMany();
+    let chipertext = await rsa.encrypt(JSON.stringify(data), pubkey);
+    chipertext = await rsa.toBase64(chipertext);
+    res.status(200).json({
+      status: 'success',
+      message: 'Data berhasil didapatkan',
+      data: chipertext,
     });
+    // console.log(data);
+  } catch (error) {
+    console.log(error);
   }
 });
 
--- rsa.js
+++ rsa.js
@@ -1,116 +1,97 @@
-import fs from "fs";
+  import fs from 'fs';
 
-// fungsi untuk menghasilkan bilangan prima secara acak
-function generatePrime(min, max) {
-  let prime = 0;
-  while (!isPrime(prime)) {
-    prime = Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  return prime;
-}
-
-// fungsi untuk memeriksa apakah sebuah bilangan prima atau tidak
-function isPrime(n) {
-  if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i == 0) return false;
-  }
-  return true;
-}
-
-// fungsi untuk mencari invers modular
-function modInverse(e, phi) {
-  let m0 = phi;
-  let y = 0, x = 1;
-  if (phi == 1) return 0;
-  while (e > 1) {
-    let q = Math.floor(e / phi);
-    let t = phi;
-    phi = e % phi, e = t;
-    t = y;
-    y = x - q * y;
-    x = t;
-  }
-  if (x < 0) x += m0;
-  return x;
-}
-
-// generate key pair
-function generateKeyPair() {
-  let p = generatePrime(100, 1000);
-  let q = generatePrime(1000, 10000);
-  let n = p * q;
-  let phi = (p - 1) * (q - 1);
-  let e = 3; // bilangan e harus relatif prima dengan phi
-  while (phi % e == 0) e++;
-  let d = modInverse(e, phi);
-  return {
-    publicKey: { n, e },
-    privateKey: { n, d },
+  // fungsi untuk menghasilkan bilangan prima secara acak
+  const generatePrime = async (min, max) => {
+    let prime = 0;
+    while (!(await isPrime(prime))) {
+      prime = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    return prime;
   };
-}
 
-// enkripsi pesan
-function encrypt(plaintext, publicKey) {
-  let { n, e } = publicKey;
-  let ciphertext = "";
-  for (let i = 0; i < plaintext.length; i++) {
-    let c = plaintext.charCodeAt(i);
-    let crypted = BigInt(c) ** BigInt(e) % BigInt(n);
-    ciphertext += crypted.toString() + " ";
-  }
-  return ciphertext.trim();
-}
+  // fungsi untuk memeriksa apakah sebuah bilangan prima atau tidak
+  const isPrime = async (n) => {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) return false;
+    }
+    return true;
+  };
 
-// dekripsi pesan
-function decrypt(ciphertext, privateKey) {
-  let { n, d } = privateKey;
-  let plaintext = "";
-  let blocks = ciphertext.split(" ");
-  for (let i = 0; i < blocks.length; i++) {
-    let decrypted = BigInt(blocks[i]) ** BigInt(d) % BigInt(n);
-    plaintext += String.fromCharCode(parseInt(decrypted.toString()));
-}
-return plaintext;
-}
+  // fungsi untuk mencari invers modular
+  const modInverse = async (e, phi) => {
+    let m0 = phi;
+    let y = 0,
+      x = 1;
+    if (phi == 1) return 0;
+    while (e > 1) {
+      let q = Math.floor(e / phi);
+      let t = phi;
+      phi = e % phi;
+      e = t;
+      t = y;
+      y = x - q * y;
+      x = t;
+    }
+    if (x < 0) x += m0;
+    return x;
+  };
 
-//to base64
-function toBase64(str) {
+  // generate key pair
+  const generateKeyPair = async () => {
+    let p = await generatePrime(100, 1000);
+    let q = await generatePrime(1000, 10000);
+    let n = p * q;
+    let phi = (p - 1) * (q - 1);
+    let e = 3; // bilangan e harus relatif prima dengan phi
+    while (phi % e == 0) e++;
+    let d = await modInverse(e, phi);
+    return {
+      publicKey: { n, e },
+      privateKey: { n, d },
+    };
+  };
+
+  // enkripsi pesan
+  const encrypt = async (plaintext, publicKey) => {
+    let ciphertext = '';
+    console.log('Data dienkripsi');
+    let { n, e } = publicKey;
+
+    for (let i = 0; i < plaintext.length; i++) {
+      let c = plaintext.charCodeAt(i);
+      let crypted = BigInt(c) ** BigInt(e) % BigInt(n);
+      ciphertext += crypted.toString() + ' ';
+    }
+    ciphertext = ciphertext.trim();
+
+    return ciphertext;
+  };
+
+  // dekripsi pesan
+  const decrypt = async (ciphertext, privateKey) => {
+    let plaintext = '';
+
+    console.log('Data didekripsi');
+    let { n, d } = privateKey;
+
+    let blocks = ciphertext.split(' ');
+    for (let i = 0; i < blocks.length; i++) {
+      let decrypted = BigInt(blocks[i]) ** BigInt(d) % BigInt(n);
+      plaintext += String.fromCharCode(parseInt(decrypted.toString()));
+    }
+
+    return plaintext;
+  };
+
+  //to base64
+  const toBase64 = async (str) => {
     return Buffer.from(str).toString('base64');
-}
+  };
 
-//from base64
-function fromBase64(str) {
+  //from base64
+  const fromBase64 = async (str) => {
     return Buffer.from(str, 'base64').toString('ascii');
-}
+  };
 
-// generate key pair
-// let { publicKey, privateKey } = generateKeyPair();
-
-// // simpan kunci publik dan kunci pribadi ke dalam file
-// fs.writeFileSync("publicKey.json", JSON.stringify(toBase64(JSON.stringify(publicKey))));
-// fs.writeFileSync("privateKey.json", JSON.stringify(toBase64(JSON.stringify(privateKey))));
-
-// enkripsi pesan
-
-const publicKey = JSON.parse(fromBase64(JSON.parse(fs.readFileSync("publicKey.json"))));
-const privateKey = JSON.parse(fromBase64(JSON.parse(fs.readFileSync("privateKey.json"))));
-
-// let plaintext = "Hello World";
-
-// let ciphertext = encrypt(plaintext, publicKey);
-// ciphertext = toBase64(ciphertext);
-// console.log("Ciphertext: " + ciphertext);
-
-// // dekripsi pesan
-
-// let decrypted = fromBase64(ciphertext);
-// console.log("Decrypted: " + decrypted);
-// decrypted = decrypt(decrypted, privateKey);
-
-// console.log("Plaintext: " + decrypted);
-
-export {encrypt, decrypt, toBase64, fromBase64 };
-
-
-
+  export default { encrypt, decrypt, toBase64, fromBase64 };
